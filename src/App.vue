7349fb0ee06c128da074bbd0c9b32ba4ff8e2e97
YangJ0605 <1442122744@qq.com>
2020-04-13 14:19:04 +0800

    完成icon,button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <y-button>默认按钮</y-button>
+      <y-button type="success">成功按钮</y-button>
+      <y-button type="primary">主要按钮</y-button>
+      <y-button type="info">主要按钮</y-button>
+      <y-button type="warning">警告按钮</y-button>
+      <y-button type="danger">危险按钮</y-button>
+    </div>
+    <div class="row">
+      <y-button plain>默认按钮</y-button>
+      <y-button type="success" plain>成功按钮</y-button>
+      <y-button type="primary" plain>主要按钮</y-button>
+      <y-button type="info" plain>主要按钮</y-button>
+      <y-button type="warning" plain>警告按钮</y-button>
+      <y-button type="danger" plain>危险按钮</y-button>
+    </div>
+     <div class="row">
+      <y-button round>默认按钮</y-button>
+      <y-button type="success" round>成功按钮</y-button>
+      <y-button type="primary" round>主要按钮</y-button>
+      <y-button type="info" round>主要按钮</y-button>
+      <y-button type="warning" round>警告按钮</y-button>
+      <y-button type="danger" round icon='delete'>危险按钮</y-button>
+    </div>
+     <div class="row">
+      <y-button circle icon='delete'></y-button>
+      <y-button type="success" circle icon='success'></y-button>
+      <y-button type="success" circle icon='success'></y-button>
+      <y-button type="primary" circle icon='email'></y-button>
+      <y-button type="info" circle icon='add'></y-button>
+      <y-button type="warning" circle icon='star'></y-button>
+      <y-button type="danger" circle icon='search'></y-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 200px 200px;
+  button {
+    margin-left: 10px;
+    margin-bottom: 10px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Button from './components/button.vue'
+import Icon from './components/icon.vue'
+
+import '@/assets/css/normalize.css'
+import '@/assets/css/font.css'
 
 Vue.config.productionTip = false
+Vue.component(Button.name, Button)
+Vue.component(Icon.name, Icon)
 
 new Vue({
   render: h => h(App)
